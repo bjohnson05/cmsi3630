311d0e84cef9206065bb02eea7ddaf30528d6ce8
B.J. Johnson <bjohnson05@gmail.com>
2023-02-09 22:25:30 -0800

    updated week05 democode.py to fix bug
    also added more output annotations to indicate operations

--- Classwork/classwork05/democode.py
+++ Classwork/classwork05/democode.py
@@ -1,10 +1,10 @@
-### 
-# filename: democode.py 
+###
+# filename: democode.py
 # purpose:  show unpacking and joining
 #           show slicing
-# author:   Dr. Johnson 
+# author:   Dr. Johnson
 # date:     2023-02-09
-### 
+###
 
 # section showing the 'unpack' operation
 a = '123456789'
@@ -15,12 +15,12 @@ print( "b is: ", b )
 # section showing the 'slice' operation
 c = a[0:3]
 d = b[4:6]
-print( "c is: ", c )
-print( "d is: ", d )
+print( "c is slice of a from 0 to 3: ", c )
+print( "d is slice of b from 4 to 6: ", d )
 
 # section showing the 'join()' operation
 e = "".join(d)
-print( "e is: ", e )
+print( "e is join of d with no spaces: ", e )
 
 # section showing 'cnunkification' of a number
 CHUNK_SIZE = 3
@@ -31,19 +31,53 @@ while( j > -CHUNK_SIZE ):
    s.append( a[j:i] )
    j -= CHUNK_SIZE
    i -= CHUNK_SIZE
-print( "s is: ", s )
+print( "s is a chunkified by 3: ", s )
 
 # another chunk, but with a string that is NOT
-#  of and even number of chunks
+#  of an even number of chunks; one left
 f = '1234567891472583'
 print( "f is: ", f )
 i = len( f )
 j = len( f ) - CHUNK_SIZE
 t = []
-while( j > -CHUNK_SIZE ):
+while( j >= 0 ):
    t.append( f[j:i] )
    j -= CHUNK_SIZE
    i -= CHUNK_SIZE
 if( len(f) % CHUNK_SIZE != 0 ):
    t.append( f[0: (len(f) % CHUNK_SIZE)] )
-print( "t is: ", t )
+print( "t is f chunkified by 3 with 1 leftover: ", t )
+
+# another chunk, but with a string that is NOT
+#  of an even number of chunks; two left
+g = '12345678914725830'
+print( "g is: ", g )
+i = len( g )
+j = len( g ) - CHUNK_SIZE
+w = []
+while( j >= 0 ):
+   w.append( g[j:i] )
+   j -= CHUNK_SIZE
+   i -= CHUNK_SIZE
+if( len(g) % CHUNK_SIZE != 0 ):
+   w.append( g[0: (len(g) % CHUNK_SIZE)] )
+print( "w is g chunkified by 3 with 2 leftover: ", w )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
