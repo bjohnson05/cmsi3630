1e7756c78e3f21ddda2a7c3dfb5785924c09b948
bjohnson05 <bjohnson05@gmail.com>
2023-02-16 13:58:11 -0800

    experiment with Die.py

--- Classwork/classwork04/Die.py
+++ Classwork/classwork04/Die.py
@@ -19,7 +19,7 @@ class Die:
          self.sides = numberOfSides
 
    def roll( self ):
-      self.value = random.randint( 1, self.sides )
+      self.value = random.randint( 0, self.sides )
       return self.value
 
    def getValue( self ):
@@ -29,33 +29,34 @@ class Die:
       rep = "[{}]".format( self.value )
       return rep
 
+def main():
+   d1 = Die( 4 )
+   print( "\n   Die 1 created ~ sides: ", d1.sides )
+   print( "\n   Die 1 created ~ value: ", d1.value )
+   d1.roll()
+   print( "\n   Die 1 rolled  ~ value: ", d1.value )
+   print( "   Die 1 rolled  ~ value: ", d1.roll() )
+   print( "   Die 1 rolled  ~ value: ", d1.roll() )
+   print( "   Die 1 rolled  ~ value: ", d1.roll() )
+   print( "   Die 1 rolled  ~ value: ", d1.roll() )
+   print( "   Die 1 rolled  ~ value: ", d1.roll() )
+   print( "   Die 1 getValue() says: ", d1.getValue() )
+   print( "   Die 1 representation : ", d1.toString() )
+   d2 = Die( 17 );
+   print( "\n   Die 2 created ~ sides: ", d2.sides )
+   print( "\n   Die 2 created ~ value: ", d2.value )
+   d2.roll()
+   print( "\n   Die 1 rolled  ~ value: ", d1.value )
+   print( "   Die 2 rolled  ~ value: ", d2.roll() )
+   print( "   Die 2 rolled  ~ value: ", d2.roll() )
+   print( "   Die 2 rolled  ~ value: ", d2.roll() )
+   print( "   Die 2 rolled  ~ value: ", d2.roll() )
+   print( "   Die 2 rolled  ~ value: ", d2.roll() )
+   print( "   Die 2 getValue() says: ", d2.getValue() )
+   print( "   Die 2 representation : ", d2.toString() )
+   print( "\n   Die 1 value: ", d1.value )
+   print( "\n   Die 2 value: ", d2.value )
+   print( "   Die 1 getValue() says: ", d1.getValue() )
+   print( "   Die 1 representation : ", d1.toString() )
 
-# d1 = Die( 4 )
-# print( "\n   Die 1 created ~ sides: ", d1.sides )
-# print( "\n   Die 1 created ~ value: ", d1.value )
-# d1.roll()
-# print( "\n   Die 1 rolled  ~ value: ", d1.value )
-# print( "   Die 1 rolled  ~ value: ", d1.roll() )
-# print( "   Die 1 rolled  ~ value: ", d1.roll() )
-# print( "   Die 1 rolled  ~ value: ", d1.roll() )
-# print( "   Die 1 rolled  ~ value: ", d1.roll() )
-# print( "   Die 1 rolled  ~ value: ", d1.roll() )
-# print( "   Die 1 getValue() says: ", d1.getValue() )
-# print( "   Die 1 representation : ", d1.toString() )
-# d2 = Die( 17 );
-# print( "\n   Die 2 created ~ sides: ", d2.sides )
-# print( "\n   Die 2 created ~ value: ", d2.value )
-# d2.roll()
-# print( "\n   Die 1 rolled  ~ value: ", d1.value )
-# print( "   Die 2 rolled  ~ value: ", d2.roll() )
-# print( "   Die 2 rolled  ~ value: ", d2.roll() )
-# print( "   Die 2 rolled  ~ value: ", d2.roll() )
-# print( "   Die 2 rolled  ~ value: ", d2.roll() )
-# print( "   Die 2 rolled  ~ value: ", d2.roll() )
-# print( "   Die 2 getValue() says: ", d2.getValue() )
-# print( "   Die 2 representation : ", d2.toString() )
-# print( "\n   Die 1 value: ", d1.value )
-# print( "\n   Die 2 value: ", d2.value )
-# print( "   Die 1 getValue() says: ", d1.getValue() )
-# print( "   Die 1 representation : ", d1.toString() )
-
+# main()
