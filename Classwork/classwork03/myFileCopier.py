256615e3881fccae084d374e59c449fece3d289d
B.J. Johnson <bjohnson05@gmail.com>
2024-02-04 11:59:54 -0800

    one more update to clean things up

--- Classwork/classwork03/myFileCopier.py
+++ Classwork/classwork03/myFileCopier.py
@@ -8,32 +8,14 @@
 # date:     2023-01-04
 ###
 
-from copiersupport.sourceFile import copiersupportReader
-from copiersupport.targetFile import copiersupportWriter
-
-file_reader = copiersupportReader()
-file_writer = copiersupportWriter()
+import copiersupport.sourceFile
+import copiersupport.targetFile
 
 def main():
-   try:
-      print( "\n\nWelcome to the file duplicator!" )
-      filename = input( "Please enter the name of the file being imported: \n" )
-      copyFile = input( "Please enter the name of the file to be outputted: \n" )
+   print( "\n\n   Welcome to the file duplicator!" )
+   filename = input( "   Please enter the name of the file:" )
+   fileContent = copiersupport.sourceFile.readFile( filename )
+   copiersupport.targetFile.writeFile( filename, fileContent )
 
-      fileContentAndStatus = file_reader.readFile( filename )
-      if fileContentAndStatus[1] == False:
-         print("There was an error reading the source file!")
-         exit()
-      else:
-         print("Successfully imported source file!")
-      status = file_writer.writeFile( copyFile, fileContentAndStatus )
-      if status == False:
-         print("There was an error writing the output file!")
-         exit()
-      else:
-         print("Successfully wrote to output file!")
-
-   except Exception as exception:
-      print( "Failed to start!\n" + str(exception) )
 
 main()
